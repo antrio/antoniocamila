79cbe05b97c7d19ac67e1c8326d4bac9ceb96339
Antonio <antoniotriolo87@gmail.com>
2020-07-11 00:55:27 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -21,3 +21,32 @@ $(function() {
 
 });
 });
+
+function countDown(second,endMinute,endHour,endDay,endMonth,endYear) {
+  var now = new Date();
+  second = (arguments.length == 1) ? second + now.getSeconds() : second;
+  endYear =  typeof(endYear) != 'undefined' ?  endYear : now.getFullYear();
+  endMonth = endMonth ? endMonth - 1 : now.getMonth();  //numero del mese cominciando da 0 esempio 03- marzo
+  endDay = typeof(endDay) != 'undefined' ? endDay :  now.getDate();
+  endHour = typeof(endHour) != 'undefined' ?  endHour : now.getHours();
+  endMinute = typeof(endMinute) != 'undefined' ? endMinute : now.getMinutes();
+  var endDate = new Date(endYear,endMonth,endDay,endHour,endMinute,second+1);
+  var interval = setInterval(function() {
+    var time = endDate.getTime() - now.getTime();
+    if (time < 0) {
+      return;
+    } else {
+      var days = Math.floor(time / 864e5);
+      var hours = Math.floor(time / 36e5) % 24;
+      var minutes = Math.floor(time / 6e4) % 60;
+      var seconds = Math.floor(time / 1e3) % 60;
+      var text='</div><div>'
+      var end='</div></div><div style="float:left;">,</div>'
+      document.getElementById('mytimer').innerHTML = '<div> </div>'+
+        days+text+'days'+end+hours+text+'hours'+end+
+        minutes+'minutes'+text+end+seconds+text+'seconds' + '<div></div>';
+    }
+    now.setSeconds(now.getSeconds() + 1);
+  }, 1000);
+}
+countDown(0,30,10,18,7,2020);
